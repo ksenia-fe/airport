8a74260db073b87cc328c9c6a77f785c74ee0d52
Mona Lisa <kseniabelikova35@gmail.com>
2021-03-01 17:11:34 +0300

    .

--- src/components/flights.selectors.js
+++ src/components/flights.selectors.js
@@ -1,31 +1,5 @@
 import moment from "moment";
 
-const setFlight = (flight, direction) => {
-  const localTime =
-    direction === "arrivals" ? flight.timeToStand : flight.timeDepShedule;
-  const deppArrTime =
-    direction === "arrivals" ? flight.timeLandFact : flight.timeTakeofFact;
-
-  return {
-    id: flight.ID,
-    term: flight.term,
-    localTime: moment(localTime).format("HH:mm"),
-    destination:
-      direction === "arrivals"
-        ? flight["airportFromID.city_en"]
-        : flight["airportToID.city_en"],
-    status:
-      direction === "departures"
-        ? `Departed ${moment(deppArrTime).format("HH:mm")}`
-        : `Landed ${moment(deppArrTime).format("HH:mm")}`,
-    airline: {
-      logo: flight.airline.en.logoSmallName,
-      name: flight.airline.en.name,
-    },
-    flightNumber: flight.codeShareData[0].codeShare,
-  };
-};
-
 export const departuresSelector = (state) => {
   const flightsList = state.app.flights;
   const dateToday = moment(new Date()).format("DD-MM-YYYY");
@@ -55,3 +29,29 @@ export const arrivalsSelector = (state) => {
     )
     .map((flight) => setFlight(flight, "arrivals"));
 };
+
+function setFlight(flight, direction) {
+  const localTime =
+    direction === "arrivals" ? flight.timeToStand : flight.timeDepShedule;
+  const deppArrTime =
+    direction === "arrivals" ? flight.timeLandFact : flight.timeTakeofFact;
+
+  return {
+    id: flight.ID,
+    term: flight.term,
+    localTime: moment(localTime).format("HH:mm"),
+    destination:
+      direction === "arrivals"
+        ? flight["airportFromID.city_en"]
+        : flight["airportToID.city_en"],
+    status:
+      direction === "departures"
+        ? `Departed ${moment(deppArrTime).format("HH:mm")}`
+        : `Landed ${moment(deppArrTime).format("HH:mm")}`,
+    airline: {
+      logo: flight.airline.en.logoSmallName,
+      name: flight.airline.en.name,
+    },
+    flightNumber: flight.codeShareData[0].codeShare,
+  };
+}
